540efbe256555b3443a55f4296ba357ffec98087
Caiza Tacuri Alisson Lisbeth <84515234+alissoncaiza@users.noreply.github.com>
2025-01-27 19:57:44 -0500

    Delete equipo-medico-service/equipo-medico-service directory

--- equipo-medico-service/equipo-medico-service/src/main/java/com/example/equipomedicoservice/repository/EquipoMedicoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.equipomedicoservice.repository;
-
-import com.example.equipomedicoservice.model.EquipoMedico;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EquipoMedicoRepository extends JpaRepository<EquipoMedico, Long> {
-}
