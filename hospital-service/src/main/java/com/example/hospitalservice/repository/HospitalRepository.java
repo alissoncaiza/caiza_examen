b9a28089f38e36b06272b8a484aa9ade514d30be
Caiza Tacuri Alisson Lisbeth <84515234+alissoncaiza@users.noreply.github.com>
2025-01-27 19:57:51 -0500

    Delete hospital-service directory

--- hospital-service/src/main/java/com/example/hospitalservice/repository/HospitalRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.hospitalservice.repository;
-
-import com.example.hospitalservice.model.Hospital;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HospitalRepository extends JpaRepository<Hospital, Long> {}
